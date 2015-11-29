9b330e0dc6b5453bc6064233034a0299a8b97bba
Ludovic CHEVRAUX <ludovic.chevraux@systemil.net>
2015-11-30 00:19:52 +0100

    Fix connect, disconnect, add student model, add login page

---
+++ oauth/student.js
@@ -0,0 +1,28 @@
+var Student = function(id, token, email, name){
+  this.id = id;
+  this.token = token;
+  this.email = email;
+  this.name = name;
+  this.authenticated = true;
+  Student.add(this);
+}
+
+students_tab =  new Array();
+Student.add = function(student){
+  students_tab.push(student)
+}
+Student.get = function(id){
+  res = students_tab.every(function (element, index, array){
+    if(element.id == id){
+      out = element;
+      return false;
+    }
+    return true;
+  });
+  return out;
+}
+
+Student.getAll = function(){
+  return students_tab;
+}
+module.exports = Student;
