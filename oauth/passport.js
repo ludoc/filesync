857fa98d163c2c660c50bdd0dc2d94ca806cfb9c
Ludovic CHEVRAUX <ludovic.chevraux@systemil.net>
2015-11-30 21:16:18 +0100

    Add Twitter and Facebook auth

--- oauth/passport.js
+++ oauth/passport.js
@@ -1,12 +1,14 @@
 module.exports = function(passport) {
 
   var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+  var FacebookStrategy = require('passport-facebook').Strategy;
+  var TwitterStrategy  = require('passport-twitter').Strategy;
   var privateKey = require('./private-key');
   var Student = require('./student');
 
   // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
-      done(null, user.id);
+    done(null, user.id);
   });
 
   // used to deserialize the user
@@ -21,13 +23,44 @@ module.exports = function(passport) {
   },
   function(token, tokenSecret, profile, done) {
 
-    //console.log(profil)
     process.nextTick(function() {
       var st = new Student(profile.id, token, profile.emails[0].value, profile.displayName);
       return done(null,st);
 
-  });
+    });
   }
 ));
+
+
+passport.use(new FacebookStrategy({
+  clientID        : privateKey.facebookAuth.clientID,
+  clientSecret    : privateKey.facebookAuth.clientSecret,
+  callbackURL     : privateKey.facebookAuth.callbackURL,
+  profileFields: ["emails", "displayName", "name"],
+
+},
+
+function(token, refreshToken, profile, done) {
+  process.nextTick(function() {
+    var st = new Student(profile.id, token, profile.emails[0].value, profile.name.givenName + ' ' + profile.name.familyName);
+    return done(null,st);
+  });
+
+}));
+
+
+passport.use(new TwitterStrategy({
+  consumerKey     : privateKey.twitterAuth.clientID,
+  consumerSecret  : privateKey.twitterAuth.clientSecret,
+  callbackURL     : privateKey.twitterAuth.callbackURL
+
+},
+function(token, tokenSecret, profile, done) {
+  process.nextTick(function() {
+    var st = new Student(profile.id, token, profile.username, profile.displayName);
+    return done(null,st);
+  });
+
+}));
 
 };
--- oauth/route.js
+++ oauth/route.js
@@ -1,22 +1,35 @@
 module.exports = function(app, passport) {
 
-    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-    app.get('/auth/google/callback', passport.authenticate('google', {
-                                            successRedirect : '/',
-                                            failureRedirect : '/login'
-           }));
+  app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+  app.get('/auth/google/callback', passport.authenticate('google', {
+    successRedirect : '/',
+    failureRedirect : '/login'
+  }));
 
-    app.get('/logout', function(req, res){
-       req.logout();
-       res.redirect('/login');
-    });
-    app.get('/ludo', function(req, res){
-      console.log(req.user);
-    });
+  app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+  app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect : '/',
+    failureRedirect : '/login'
+  }));
+  app.get('/auth/twitter', passport.authenticate('twitter'));
+  app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', {
+    successRedirect : '/',
+    failureRedirect : '/login'
+  }));
 
-    app.get('/login', function(req, res) {
-      var path = require('path');
-      res.sendFile(path.resolve('public/login.html'));
-    });
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/login');
+  });
+  app.get('/ludo', function(req, res){
+    console.log(req.user);
+  });
 
-  };
+  app.get('/login', function(req, res) {
+    var path = require('path');
+    res.sendFile(path.resolve('public/login.html'));
+  });
+
+};
