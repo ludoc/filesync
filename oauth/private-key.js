c475cb38fa9d72c9f4949536d0f33910ab33a40f
Ludovic CHEVRAUX <ludovic.chevraux@systemil.net>
2015-11-30 21:19:02 +0100

    Add private-key.js structure

---
+++ oauth/private-key.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  'googleOauth' : {
+    'clientID' : '',
+    'clientSecret' : '',
+    'callbackURL' : 'http://localhost:3000/auth/google/callback'
+  },
+
+  'facebookAuth' : {
+    'clientID' : '',
+    'clientSecret' : '',
+    'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+  },
+  'twitterAuth' : {
+    'clientID' : '',
+    'clientSecret' : '',
+    'callbackURL' : 'http://localhost:3000/auth/twitter/callback'
+  }
+};
